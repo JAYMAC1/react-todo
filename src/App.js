934a5bd0392ed8043500a92768c94a49acbc7967
James McNeil <mcneil_james@hotmail.com>
2022-03-30 20:04:53 +0100

    created sform with input and button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  return <div className='App'>React To-do</div>
+  const [todo, setTodo] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(todo)
+    setTodo('')
+  }
+
+  return (
+    <div className='App'>
+      <div className='form-container'>
+        <form className='todo-form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <input
+              type='text'
+              placeholder='Enter a new todo'
+              onChange={(e) => setTodo(e.target.value)}
+              value={todo}
+            />
+          </div>
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
