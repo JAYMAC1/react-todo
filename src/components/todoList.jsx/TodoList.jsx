ddeceb17614bf6dfca4b514176c873708a4a73d2
James McNeil <mcneil_james@hotmail.com>
2022-03-31 08:04:02 +0100

    import TodoItem

--- src/components/todoList.jsx/TodoList.jsx
+++ src/components/todoList.jsx/TodoList.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import TodoItem from '../todoItem/TodoItem'
+import './TodoList.css'
 
-const TodoList = () => {
-  return <div>TodoList</div>
+const TodoList = ({ todos }) => {
+  return (
+    <div className='todo-container'>
+      <div className='todo-display'>
+        <TodoItem />
+      </div>
+    </div>
+  )
 }
 
 export default TodoList
